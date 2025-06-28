b89fc4b0345f79f519980aa391e68f43ebfefe28
Brian Lee <jm3lee@gmail.com>
2025-06-27 23:26:19 -0700

    squash

--- dep.mk
+++ dep.mk
@@ -60,11 +60,3 @@ build/%.json: %.json
 	emojify < $< > $@
 
 build/background/key_terms.md: build/background/key_terms.json
-
-build/%.yml: %.yml | $(patsubst src/%,build/%,$(dir $@))
-	cp $< $@
-
-build/toc.html: build/bones/femur/toc.yml
-build/toc.html: build/bones/humerus/toc.yml
-build/toc.html: build/bones/radius/toc.yml
-build/toc.html: build/bones/scapula/toc.yml
